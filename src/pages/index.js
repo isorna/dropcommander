e38957b74037521905d08336013aa8175b521d48
Alvaro Isorna <alvaro.isorna@gmail.com>
2022-06-11 12:58:31 +0200

    i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
+import Translate from '@docusaurus/Translate';
 // import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 function HomepageHeader() {
@@ -11,13 +12,13 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title"><Translate>Drop Commander</Translate></h1>
+        <p className="hero__subtitle"><Translate>Dropzone &amp; Dropfleet Commander fan site</Translate></p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Introduction
+            <Translate>Introduction</Translate>
           </Link>
         </div>
       </div>
