472b02ee715df9f037415121ce9346d5452e83ef
Alvaro Isorna <alvaro.isorna@gmail.com>
2023-05-19 14:30:34 +0200

    feat(pwa): :sparkles: integrate pwa with vite-pwa

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitepress'
+import { withPwa } from '@vite-pwa/vitepress'
 import { enConfig } from './en'
 import { esConfig } from './es'
 import { shareConfig } from './share'
 
-export default defineConfig({
+export default withPwa(defineConfig({
   ...shareConfig,
   locales: {
     // root: { label: 'English', lang: 'en-US' },
@@ -20,4 +21,65 @@ export default defineConfig({
     en: { label: 'English', lang: 'en-US', ...enConfig },
     es: { label: 'Español', lang: 'es-ES', ...esConfig },
   },
-})
+  pwa: {
+    registerType: 'autoUpdate',
+    includeAssets: [
+      'favicon.ico',
+      '/img/safari-pinned-tab.svg',
+      '/img/android-chrome-192x192.png',
+      '/img/android-chrome-512x512.png',
+      '/img/apple-touch-icon.png',
+      '/img/favicon-16x16.png',
+      '/img/mstile-150x150.png',
+    ],
+    manifest: {
+      name: 'Drop Commander',
+      short_name: 'DC',
+      start_url: '/',
+      icons: [
+          {
+              src: '/img/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+          },
+          {
+              src: '/img/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+          }
+      ],
+      theme_color: '#3b9235',
+      background_color: '#ffffff',
+      display: 'standalone',
+      orientation: 'portrait-primary',
+      shortcuts: [
+        {
+          name: 'Dropfleet Commander',
+          short_name: 'DFC',
+          description: 'DFC Rules',
+          url: 'en/dfc/',
+          icons: [
+            {
+              src: '/img/ui/orbit.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        },
+        {
+          name: 'Dropzone Commander',
+          short_name: 'DZC',
+          description: 'DZC Rules',
+          url: 'en/dzc/',
+          icons: [
+            {
+              src: '/img/ui/moon-landing.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}))
